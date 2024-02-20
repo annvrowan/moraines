d4d47d2f75ceee53d669ea0bdf18a429bc56abbb
annvrowan <ann.rowan@uib.no>
2024-02-20 13:40:08 +0100

    improved handling of database connection

--- iced_query.m
+++ iced_query.m
@@ -6,25 +6,25 @@
 %Before running this script, some prep is needed:
 
 %Download the Java driver from https://dev.mysql.com/downloads/connector/j/
-%(select 'platform independent' for OS X and mysql-connector-j-8.2.0.jar, or latest version)
+%(select 'platform independent' for OS X and mysql-connector-j-8.3.0.jar, or latest version)
 %Copy/paste your driver jar file to a local directory, and note the
 %address.
 
-%Install MATLAB Database Explorer.
+%Install the MATLAB Database Explorer toolbox.
 
 %Make an ssh tunnel connection to the database, on OS X using Terminal:
 %>> ssh -f iced@stoneage.ice-d.org -L 12345:34.73.248.9:3306 -N
 % and wait a few seconds for it to return done.
 
 
-%To get started use one of these two options to make a connection to
+%Script section 1: To get started, use one of these two options (option 1 and option 2) to make a connection to
 %the database:
 
-%1. Open MATLAB Database Explorer:
+%Option 1. Open MATLAB Database Explorer:
 % Select 'Configure Data Source' and 'Configure JDBC Data Source'.
 % Populate the fields in pop-up menu:
 % Give the source a name (e.g., "iced_connect")
-% Driver Location: <local address>/mysql-connector-j-8.2.0.jar
+% Driver Location: <local address>/mysql-connector-j-8.3.0.jar
 % Vendor = MySQL
 % Database = "iced"
 % PortNumber = "12345"
@@ -34,26 +34,40 @@
 % 'database'
 % function call below (about line 60). Then you can run this script to access the database.
 
-
-%2. As an alternative to option 1, you can instead put the connection info into the
+%Option 2. As an alternative to option 1, you can instead put the connection info into the
 %'database' function below (about line 68). The java path needs to match the location of the downloaded driver.
 
-
 %for more info on connecting to ICED, see: https://wiki.ice-d.org/applications:connect_matlab_mac
+
+% Script section 2: query the database using the parameters that you
+% require, using the same format as when you connect to the database
+% directly.
+
 %for more info on writing good queries, see: https://wiki.ice-d.org/pluginto:useful_sql_queries
+
+%Then the sample ages can either be;
+% Script section 3: collected from the database, which are updated daily and use a standard production rate calibration, or;
+% Script section 4: sample data can be used to produce recalibrated ages using cosmo_calculator, e.g., to adjust calibration parameters.
+
+%When you are finished using the database, use Terminal to close the
+%tunnel:
+%Run this command to find connection 
+% >> ps aux | grep 3306
+%and then this command to close the connection:
+% >> kill <connection number>
 
 % Notes:
 %Version 1 took 500 s to run 830 NZ sample ages
 %Version 2 took 200 s to run 934 NZ sample ages
 
 %Created by Greg Balco
-%Last modifed by Ann Rowan on 10/01/24
+%Last modifed by Ann Rowan on 20/02/24
 
 
 clear all
 close all
 tic
-%% Connect to the database
+%% 1. Connect to the database
 %Both options assume that there is a connection to the database server, so an SSH tunnel needs to be open to the database.
 %Note use of read-only login: reader/beryllium-10
 
@@ -64,11 +78,11 @@ tic
 [sys1,sys2] = system('ps aux | grep 3306');
 portindex = strfind(sys2,':34.73.248.9:');
 portstr = sys2((portindex(1)-5):(portindex(1)-1));
-javaaddpath([matlabroot,'/java/jarext/mysql-connector-j-8.2.0.jar'])
+javaaddpath([matlabroot,'/java/jarext/mysql-connector-j-8.3.0.jar'])
 configureJDBCDataSource("Vendor",'MySQL');
 dbc = database('iced','reader','beryllium-10','Vendor','MySQL','Server','localhost','port',str2num(portstr));
 
-%% Query ICED to return a list of sample names
+%% 2. Query ICED to return a list of sample names
 %Make a MySQL query to return a list of all samples from moraines in the Southern Alps
 q1 = ['select base_site.short_name, base_sample.name, base_sample.lon_DD,base_sample.lat_DD,base_sample.elv_m, base_continent.name ' ...
     'from base_sample join base_site on base_site.id = base_sample.site_id join base_continent on base_site.continent_id = base_continent.id ' ...
@@ -83,7 +97,10 @@ no_samples = length(name_list);
 %Print number of samples found
 disp(strcat("Number of samples = ",num2str(no_samples)))
 
-%% Collect sample data for all samples indentified and put into a cell array
+
+return
+
+%% 3. Collect sample data for all samples indentified and put into a cell array
 % Sample name needs to have this format to query the database: '"Barr2007-A-WH-01B"'
 sample_data = cell(no_samples,9);
 %Populate the table with the sample name in col1 and the string for cosmo
@@ -95,11 +112,17 @@ for i = 1:no_samples
     sample_data{i,1} = name;
     sample_data{i,2} = fetch(dbc,q2,'DataReturnFormat','cellarray');
 end
+
+
+
 %Now close the database connection
 close(dbc);
 toc
 
-%% Send sample data to cosmo calculator and return age and age errors
+
+
+
+%% 4. Send sample data to cosmo calculator and return age and age errors
 %This part of the script calls a different script; cosmo_calculator
 %The script returns the sample names into col3 of the table so they can be
 %checked against the original names in col1 as q quick check that the data
